44a524943efe5022a1da660ed15f534d592237f6
Niv <bniv000@gmail.com>
2022-10-02 12:50:14 +0300

    fixed trips bug and img

--- src/cmps/reservation-confirmation.jsx
+++ src/cmps/reservation-confirmation.jsx
@@ -2,7 +2,6 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import CloseIcon from '@mui/icons-material/Close';
-import StarIcon from '@mui/icons-material/Star'
 import { Link } from 'react-router-dom';
 import { Loader } from './loader'
 
@@ -25,57 +24,49 @@ const style = {
 
 
 
-export const ConfirmationModal = ({ stay, order, confirm , closeConfirm, userId, calcNights}) => {
+export const ConfirmationModal = ({ stay, order, confirm, closeConfirm, userId, calcNights }) => {
 
-    if(!order) return <Loader />
+    if (!order) return <Loader />
     return <Modal
-            open={confirm}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description">
-            <Box sx={style}>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                    <div className='confirmation-header'>
-                    <p className='section-four-total'><span className='close-btn-confirmation' onClick={() => closeConfirm(false)}><CloseIcon fontSize='medium'/></span></p>
-
-                        <p className='confirmation-header-txt'>We received your order</p>
+        open={confirm}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description">
+        <Box sx={style}>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                <div className='confirmation-header'>
+                    <p className='section-four-total'><span className='close-btn-confirmation' onClick={() => closeConfirm(false)}><CloseIcon fontSize='medium' /></span></p>
+                    <p className='confirmation-header-txt'>We received your order</p>
+                </div>
+                <div className='sub-txt-main'>
+                    <div className='confirmation-img-container'>
+                    <img style={{ width: '100%' }} className='confirmation-img' src={stay.imgUrls[0]} alt="preview " />
                     </div>
-                    <div className='sub-txt-main'>
-                        <div>
-                                <img  style={{width:'100%'}} className='confirmation-img' src={stay.imgUrls[0]} alt="preview " />
-                            <div className='confirmation-img-container' >
-
-                           
-                            </div>
-                        </div>
-
-
-                        <div className='inside-card'>
-                            <p className='section-one-header' >Your Trip</p>
-                            <p className='section-one-trip'> <span >Dates</span> <span>{`${order.startDate} - ${order.endDate}`} </span> </p>
-                            <p className='section-one-trip'> <span >Location</span> <span>{stay.name} </span> </p>
-                            {/* <p className='section-one-guests'>  <span>Guests</span>{order.guests}</p> */}
-                        </div>
-                    
-
-                        <div className='inside-card'>
-                            <p className='section-two-header'>Price Details</p>
-                            <p className='section-two-price'> ${order.stay.price * calcNights().toFixed(0)}
-                                <span className='price-section'> ${order.stay.price} x  {calcNights().toFixed(0)} nights</span>
-                            </p>
-                            <p className='section-two-fee'> $0
-                                <span className='price-section'>Service fee</span></p>
-                        </div>
-                        <div className='inside-card'>
-                            <p className='section-three-total'>
-                                <span>Total</span>
-                                <span className='total'>${order.stay.price * calcNights().toFixed(0)} </span>
-                            </p>
-                        </div>
-                        <div className='inside-card-4'>
-                            <Link className='back-to-trips' to={`/trips/${userId}`}>Go to my trips</Link>
-                        </div>
+                    <div className='inside-card'>
+                        <p className='section-one-header' >Your Trip</p>
+                        <p className='section-one-trip'> <span >Dates</span> <span>{`${order.startDate} - ${order.endDate}`} </span> </p>
+                        <p className='section-one-trip'> <span >Location</span> <span>{stay.name} </span> </p>
                     </div>
-                </Typography >
-            </Box>
-        </Modal >
+                    <div className='inside-card'>
+                        <p className='section-two-header'>Price Details</p>
+                        <p className='section-two-price'> 
+                            ${order.stay.price * calcNights().toFixed(0)}
+                            <span className='price-section'> ${order.stay.price} x  {calcNights().toFixed(0)} nights</span>
+                        </p>
+                        <p className='section-two-fee'> $0
+                            <span className='price-section'>Service fee</span>
+                        </p>
+                    </div>
+                    <div className='inside-card'>
+                        <p className='section-three-total'>
+                            <span>Total</span>
+                            <span className='total'>${order.stay.price * calcNights().toFixed(0)} </span>
+                        </p>
+                    </div>
+                    <div className='inside-card-4'>
+                        <Link className='back-to-trips' to={`/trips/${userId}`}>Go to my trips</Link>
+                    </div>
+                </div>
+            </Typography >
+        </Box>
+    </Modal >
 }
--- src/views/trips.jsx
+++ src/views/trips.jsx
@@ -2,7 +2,6 @@ import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { loadOrders } from "../store/order.action"
 import { Loader } from '../cmps/loader'
-import { utilService } from '../services/util.service'
 import HourglassEmptyIcon from '@mui/icons-material/HourglassEmpty';
 import DoneOutlineIcon from '@mui/icons-material/DoneOutline';
 import HighlightOffOutlinedIcon from '@mui/icons-material/HighlightOffOutlined';
@@ -13,40 +12,33 @@ export const Trips = () => {
   const orders = useSelector(state => state.orderModule.orders)
   const dispatch = useDispatch()
 
-
-  console.log('orders:' , orders)
   
+  // useEffect(() => {
+  //   changeLayout('1270px')
+  //   return () => {
+  //     changeLayout('1760px')
+  //   }
+  // }, [])
+  // const changeLayout = (value) => {
+  //   document.documentElement.style.setProperty('--layoutWidth', value);
+  // }
+
   useEffect(() => {
-    changeLayout('1270px')
-    return () => {
-      changeLayout('1760px')
-    }
-  }, [])
-
-  const changeLayout = (value) => {
-    document.documentElement.style.setProperty('--layoutWidth', value);
-  }
-
-  useEffect(() => {
-    getOrders()
-  }, [])
-
-  const getOrders = () => {
     dispatch(loadOrders())
-  }
+  }, [])
 
   if (!orders) return <Loader />
 
   return <section className="trips">
-    <h1 className='trips-title'>Trips</h1>
+    <h1 className='trips-title'>My trips</h1>
     <ul className="trips-grid-container">
       {orders.reverse().map((order) => {
           return <li key={order._id}>
-            <img src={require(`../assets/imgs/preview-imgs/${utilService.getRandomIntInclusive(1, 20)}.webp`)} alt="host-img" />
+            {order.stay && <img src={order.stay.img} alt="host-img" />}
             <div className="trip-text-container">
               <div className="upper-section">
-                <h4>{order.stay.name}</h4>
-                <p>{`Hosted by Kevin`}</p>
+                {order.stay && <h4>{order.stay.name}</h4>}
+                <p>Hosted by Kevin</p>
                 <p>{`${order.startDate} - ${order.endDate}`}</p>
               </div>
 
@@ -77,8 +69,3 @@ export const Trips = () => {
     </ul>
   </section >
 }
-
-{/* <div className="bottom-section">
-<i>{order.status === 'approved' ? < DoneOutlineIcon color="success" fontSize="small" /> : <HourglassEmptyIcon sx={{ color: 'rgb(255, 211, 0)' }} fontSize="small" />} </i>
-<p>{order.status}</p>
-</div> */}
